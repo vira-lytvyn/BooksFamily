cb659618fa58dfab790ead5278cabf4138b1d419
Vira Lytvyn <baterfluy@gmail.com>
2015-03-18 00:29:53 +0200

    Some simple exercises with PouchDB.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+PouchDB.debug.enable('*');
+
+var db = new PouchDB('http://localhost:5984/books');
+
+db.info().then(function (info) {
+  console.log(info);
+});
+
+var doc = {
+  '_id': '111-111-111-111',
+  'name': 'Test book name 1',
+  'authors': ['Name Surname'],
+  'year': 2015,
+  'janres': [
+    'novel',
+    'romantic',
+    'historical'
+  ]
+};
+
+db.put(doc);
+
+db.get('111-111-111-111').then(function (doc) {
+  console.log(doc);
+});
+
+// fetch 111-111-111-111
+db.get('111-111-111-111').then(function (doc) {
+  // update his year
+  doc.year = 2014;
+  // put him back
+  return db.put(doc);
+}).then(function () {
+  // fetch 111-111-111-111 again
+  return db.get('111-111-111-111');
+}).then(function (doc) {
+  console.log(doc);
+});
