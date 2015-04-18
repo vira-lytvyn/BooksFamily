185fe1ce7f092e9b4056ce66f0ae7f3804169b61
Vira Lytvyn <baterfluy@gmail.com>
2015-04-18 15:03:06 +0300

    Added book route.

---
+++ client/app/book/book.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('booksFamilyApp')
+  .controller('BookCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/book/book.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('booksFamilyApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('book', {
+        url: '/book',
+        templateUrl: 'app/book/book.html',
+        controller: 'BookCtrl'
+      })
+      .state('create', {
+        url: '/newBook',
+        templateUrl: 'app/bookForm/bookForm.html',
+        controller: 'BookCtrl'
+      }
+      .state('edit', {
+        url: '/book/:id',
+        templateUrl: 'app/bookForm/bookForm.html',
+        controller: 'BookCtrl'
+      });
+  });
