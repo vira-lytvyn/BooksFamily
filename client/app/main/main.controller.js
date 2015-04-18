dca9d62c487f8d3cb6b1fae80d5d0b839589f187
Vira Lytvyn <baterfluy@gmail.com>
2015-04-18 15:04:31 +0300

    Updated homepage to contain books info instead of test data.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,26 +2,26 @@
 
 angular.module('booksFamilyApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    $scope.newestBooks = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+    $http.get('/api/books').success(function(newestBooks) {
+      $scope.newestBooks = newestBooks;
+      socket.syncUpdates('book', $scope.newestBooks);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    $scope.addBook = function() {
+      if($scope.newBook === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/books', { name: $scope.newBook });
+      $scope.newBook = '';
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.deleteBook = function(book) {
+      $http.delete('/api/books/' + book._id);
     };
 
     $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+      socket.unsyncUpdates('book');
     });
   });
